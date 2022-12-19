f3c5d3221e0735d31ff2b33e0a666f97b40c1b09
Jose <josemgomez40534@gmail.com>
2022-12-19 17:20:11 -0300

    a

--- ayuda desafio/clase23/inyectarCookies/rutas/cookiesRuta.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { setCookie, getCookie, clearCookie } = require('../controller/cookiesController');
-
-router.get('/create', setCookie);
-router.get('/get', getCookie);
-router.get('/delete', clearCookie);
-
-module.exports = router;
--- ayuda desafio/clase24/sesiones_usuario_servidor/rutas/sesionRuta.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { root, olvidar } = require('../controller/sessionController');
-
-router.get('/root', root);
-router.get('/olvidar', olvidar);
-
-module.exports = router;
